24ca41f78a51f966180bd34c2a0db57a7bffcad8
benjamindrake <spcbdrake@yahoo.com>
2015-12-02 10:53:25 -0500

    DO YOU WANT ANTS BECAUSE THAT'S HOW YOU GET ANTS

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :main ants-clojure.core
+  :aot [ants-clojure.core]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/ants_clojure/core.clj
+++ src/ants_clojure/core.clj
@@ -4,11 +4,40 @@
            [javafx.fxml FXMLLoader]
            [javafx.stage Stage]
            [javafx.scene Scene]
-           [javafx.animation AnimationTimer])
+           [javafx.animation AnimationTimer]
+           (javafx.scene.paint Color))
   (:gen-class :extends javafx.application.Application))
 
 (def width 800)
 (def height 600)
+(def ant-count 100)
+(def ants (atom nil))
+(def last-timestamp (atom 0))
+
+
+(defn create-ants []
+  (for [i (range 0 ant-count)]
+    {:x (rand-int width )
+     :y (rand-int height)}))
+
+(defn random-step []
+  (- (* 2 (rand)) 1))
+
+(defn move-ant [ant]
+  (Thread/sleep 1)
+  (assoc ant :x (+ (random-step) (:x ant))
+             :y (+ (random-step) (:y ant))))
+
+(defn draw-ants [context]
+  (.clearRect context 0 0 width height)
+  (doseq [ant (deref ants)]
+    (.setFill context Color/BLUE)
+    (.fillOval context (:x ant) (:y ant) 5 5)))
+
+(defn fps [now]
+  (let [diff (- now (deref last-timestamp))
+        diff-seconds (/ diff 1000000000)]
+    (int (/ 1 diff-seconds))))
 
 (defn -start [app ^Stage stage]
   (let [root (FXMLLoader/load (resource "main.fxml"))
@@ -18,7 +47,11 @@
         context (.getGraphicsContext2D canvas)
         timer (proxy [AnimationTimer] []
                 (handle [now]
-                  ))]
+                  (.setText fps-label (str (fps now)))
+                  (reset! last-timestamp now)
+                  (reset! ants (pmap move-ant (deref ants)))
+                  (draw-ants context)))]
+    (reset! ants (create-ants))
     (doto stage
       (.setTitle "Ants")
       (.setScene scene)
